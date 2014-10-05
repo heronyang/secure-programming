fde36aa348374d565f844868f8e2c8907ff9f539
Heron Yang <v6510young88@gmail.com>
2014-10-05 17:22:51 +0800

    trying 05

--- wargame/05/test.py
+++ wargame/05/test.py
@@ -1,2 +1,16 @@
-#print 'login\nadmin\n\x00DoYouThinkThisIsPassword\x00'
-print 'login\nadmin\n\xE1\x61\xE1\x61\x00'
+GUESS = 5
+
+m1_o = 'Dunipw'
+m2 = 'hYexBC'
+
+s = 'ooTikhssasod'
+m1 = ''
+for i in range(len(m1_o)):
+    m1 = m1 + chr(ord(m1_o[i])^GUESS)
+print 'm1: ' + m1
+
+pwd = ''
+for i in range(6):
+    pwd = pwd + m1[i] + s[i*2] + m2[i] + s[i*2+1]
+
+print 'login\nadmin\n' + pwd + '\n'

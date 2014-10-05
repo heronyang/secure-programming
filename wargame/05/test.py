338745d2075408a7a1e903f408efc684ca4fba44
Heron Yang <v6510young88@gmail.com>
2014-10-05 17:28:06 +0800

    start to force try

--- wargame/05/test.py
+++ wargame/05/test.py
@@ -1,4 +1,4 @@
-GUESS = 5
+GUESS = 61
 
 m1_o = 'Dunipw'
 m2 = 'hYexBC'
@@ -7,10 +7,9 @@ s = 'ooTikhssasod'
 m1 = ''
 for i in range(len(m1_o)):
     m1 = m1 + chr(ord(m1_o[i])^GUESS)
-print 'm1: ' + m1
 
 pwd = ''
 for i in range(6):
     pwd = pwd + m1[i] + s[i*2] + m2[i] + s[i*2+1]
 
-print 'login\nadmin\n' + pwd + '\n'
+print 'login\nadmin\n' + pwd + '\nflag\n'

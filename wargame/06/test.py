f1d71ca8673691f73ea22909af6ae8261b521b4b
Heron Yang <v6510young88@gmail.com>
2014-10-09 00:37:53 +0800

    update

--- wargame/06/test.py
+++ wargame/06/test.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python
 from subprocess import call
-call(['./oc', '1', '2', '3', '4', '5', '6', 'A'*16 + '\x30\x87\x04\x08'])
+argv = ['1', '2', '3', '4', '5', '6', 'A'*16 + '\x30\x87\x04\x08']
+for i in argv:
+    print i + " ";
